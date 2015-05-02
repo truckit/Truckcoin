d05be20a0cafad122244d5b5535beef510952923
noise23 <admin@acidpool.com>
2015-05-02 13:36:21 +0300

    CAlert: only mark misbehaving if signature is bad

--- src/main.cpp
+++ src/main.cpp
@@ -3538,6 +3538,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
                 // This isn't a Misbehaving(100) (immediate ban) because the
                 // peer might be an older or different implementation with
                 // a different signature key, etc.
+				if(!alert.CheckSignature())
                 pfrom->Misbehaving(10);
             }
         }
