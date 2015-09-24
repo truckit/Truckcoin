48b843420dea02587bcac4ea4da18e57e5f47b93
noise23 <admin@acidpool.com>
2015-09-24 13:29:55 +0300

    Fix OSX build

--- src/qt/notificator.cpp
+++ src/qt/notificator.cpp
@@ -15,7 +15,10 @@
 #include <QtDBus/QtDBus>
 #include <stdint.h>
 #endif
-
+// Include ApplicationServices.h after QtDbus to avoid redefinition of check().
+// This affects at least OSX 10.6. See /usr/include/AssertMacros.h for details.
+// Note: This could also be worked around using:
+// #define __ASSERT_MACROS_DEFINE_VERSIONS_WITHOUT_UNDERSCORES 0
 #ifdef Q_OS_MAC
 #include <ApplicationServices/ApplicationServices.h>
 #include "macnotificationhandler.h"
@@ -65,6 +68,7 @@ Notificator::Notificator(const QString &programName, QSystemTrayIcon *trayicon, 
             }
             CFRelease(cfurl);
             CFRelease(bundle);
+    }
     }
 #endif
 }
