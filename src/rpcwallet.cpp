b9b836437e7ee4fbbebff66d0a64ca6c511b2c83
noise23 <admin@acidpool.com>
2019-07-16 04:01:51 +0300

    Small changes to getinfo and getmininginfo rpc

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -83,8 +83,8 @@ Value getinfo(const Array& params, bool fHelp)
     obj.push_back(Pair("proxy",         (proxy.first.IsValid() ? proxy.first.ToStringIPPort() : string())));
     obj.push_back(Pair("ip",            addrSeenByPeer.ToStringIP()));
 //    obj.push_back(Pair("PoW difficulty",    (double)GetDifficulty()));
-	obj.push_back(Pair("PoS difficulty", GetDifficulty(GetLastBlockIndex(pindexBest, true))));
-	obj.push_back(Pair("split threshold",   ValueFromAmount(nSplitThreshold)));
+	obj.push_back(Pair("difficulty", GetDifficulty(GetLastBlockIndex(pindexBest, true))));
+	obj.push_back(Pair("splitthreshold",   ValueFromAmount(nSplitThreshold)));
     obj.push_back(Pair("testnet",       fTestNet));
     obj.push_back(Pair("keypoololdest", (boost::int64_t)pwalletMain->GetOldestKeyPoolTime()));
     obj.push_back(Pair("keypoolsize",   pwalletMain->GetKeyPoolSize()));
