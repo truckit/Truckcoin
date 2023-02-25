9a56e5a0f8086645d38eb7b53dd413a22b3aa632
noise23 <noise23@users.noreply.github.com>
2023-02-25 02:22:06 +0200

    Add CScriptCheck: a closure representing a script check

--- src/main.h
+++ src/main.h
@@ -488,7 +488,6 @@ enum GetMinFee_mode
     GMF_SEND,
 };
 
-// Modes for script/signature checking
 enum CheckSig_mode
 {
     CS_NEVER,             // never validate scripts
@@ -1192,6 +1191,34 @@ public:
             if (!out.IsNull())
                 return false;
         return true;
+    }
+};
+
+/** Closure representing one script verification
+ *  Note that this stores references to the spending transaction */
+class CScriptCheck
+{
+private:
+    CScript scriptPubKey;
+    const CTransaction *ptxTo;
+    unsigned int nIn;
+    unsigned int nFlags;
+    int nHashType;
+
+public:
+    CScriptCheck() {}
+    CScriptCheck(const CCoins& txFromIn, const CTransaction& txToIn, unsigned int nInIn, unsigned int nFlagsIn, int nHashTypeIn) :
+        scriptPubKey(txFromIn.vout[txToIn.vin[nInIn].prevout.n].scriptPubKey),
+        ptxTo(&txToIn), nIn(nInIn), nFlags(nFlagsIn), nHashType(nHashTypeIn) { }
+
+    bool operator()() const;
+
+    void swap(CScriptCheck &check) {
+        scriptPubKey.swap(check.scriptPubKey);
+        std::swap(ptxTo, check.ptxTo);
+        std::swap(nIn, check.nIn);
+        std::swap(nFlags, check.nFlags);
+        std::swap(nHashType, check.nHashType);
     }
 };
 
