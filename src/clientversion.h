2053e2c49fe17661806dedaf2f59f38ff265e19a
noise23 <noise23@users.noreply.github.com>
2023-06-07 01:12:43 +0300

    v2.2.5.0

--- src/clientversion.h
+++ src/clientversion.h
@@ -8,7 +8,7 @@
 // These need to be macros, as version.cpp's and truckcoin-qt.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR       2
 #define CLIENT_VERSION_MINOR       2
-#define CLIENT_VERSION_REVISION    0
+#define CLIENT_VERSION_REVISION    5
 #define CLIENT_VERSION_BUILD       0
 
 // Converts the parameter X to a string after macro replacement on X has been performed.
@@ -17,6 +17,6 @@
 #define DO_STRINGIZE(X) #X
 
 // Copyright year
-#define COPYRIGHT_YEAR  2020
+#define COPYRIGHT_YEAR  2023
 
 #endif // CLIENTVERSION_H
