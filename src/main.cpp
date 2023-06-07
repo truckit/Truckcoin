4674cac6548a4c29a7ecd4724a1cb3069b9d2437
noise23 <noise23@users.noreply.github.com>
2023-06-07 03:49:02 +0300

    script: Switch outside users to CScriptNum.
    https://github.com/bitcoin/bitcoin/commit/4f497cd97da4f9c88790a5f4e97926804669dfca

--- src/main.cpp
+++ src/main.cpp
@@ -2978,7 +2978,7 @@ bool LoadBlockIndex()
         txNew.nTime = nChainStartTime;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
-        txNew.vin[0].scriptSig = CScript() << 486604799 << CBigNum(9999) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
+        txNew.vin[0].scriptSig = CScript() << 486604799 << CScriptNum(9999) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
         txNew.vout[0].SetEmpty();
 
         CBlock block;
